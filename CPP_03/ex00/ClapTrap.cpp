a5d3133914d5f92b123ce35d4c3f0b3635921a8f
b.daribazaron <b.daribazaron@innopolis.ru>
2022-02-04 03:20:55 +0300

    cpp03 ex00

--- CPP_03/ex00/ClapTrap.cpp
+++ CPP_03/ex00/ClapTrap.cpp
@@ -1,14 +1,21 @@
 
 #include "ClapTrap.h"
 
+ClapTrap::ClapTrap()
+{
+	energyPoints = 10;
+	hitPoints = 10;
+	attackDamage = 0;
+	std::cout << "ClapTrap created..." << std::endl;
+}
 
 ClapTrap::ClapTrap(std::string ct_name)
 {
-	std::cout << "ClapTrap " << ct_name << " created..." << std::endl;
 	name = ct_name;
 	energyPoints = 10;
 	hitPoints = 10;
 	attackDamage = 0;
+	std::cout << "ClapTrap " << ct_name << " created..." << std::endl;
 }
 
 ClapTrap::ClapTrap(ClapTrap &ct)
@@ -32,17 +39,18 @@ ClapTrap &ClapTrap::operator=(const ClapTrap &ct)
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-	if (energyPoints > 0)
+	if (energyPoints > 0 && hitPoints > 0)
 	{
 		energyPoints--;
 		hitPoints += amount;
-		std::cout << "ClapTrap " << name << " repairs for " << amount << " hit points!" << std::endl;
+		std::cout << "ClapTrap " << name << " repairs for "
+		<< amount << " hit points! Current hit points is " << hitPoints << std::endl;
 	}
 }
 
 void ClapTrap::attack(const std::string &target)
 {
-	if (energyPoints < 0)
+	if (energyPoints == 0 || hitPoints == 0)
 		return;
 	energyPoints--;
 	std::cout << "ClapTrap " << name << " attacks " << target << ", causing "
@@ -51,5 +59,12 @@ void ClapTrap::attack(const std::string &target)
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-	(void) amount;
+	if (energyPoints == 0 || hitPoints == 0)
+		return;
+	if (hitPoints > amount)
+		hitPoints -= amount;
+	else
+		hitPoints = 0;
+	std::cout << "ClapTrap " << name << " takes " << amount <<
+	" damage! Current hit points is " << hitPoints << std::endl;
 }
--- CPP_03/ex00/main.cpp
+++ CPP_03/ex00/main.cpp
@@ -2,7 +2,12 @@
 
 int main( void )
 {
-	ClapTrap ct("Bulat");
+	ClapTrap ct("Warrior");
+	ClapTrap copy_ct(ct);
 	ct.beRepaired(10);
-	ct.attack("10");
+	ct.attack("10___");
+	ct.takeDamage(24);
+	ct.beRepaired(10);
+	copy_ct.attack("Other");
+	copy_ct.takeDamage(9);
 }
