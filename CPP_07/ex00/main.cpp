ae21e96f6e38eb0b5163638c12cf2cf7e2c330cf
bulat <b.daribazaron@innopolis.ru>
2022-02-15 16:54:58 +0300

    cpp07 ex00

--- CPP_07/ex00/main.cpp
+++ CPP_07/ex00/main.cpp
@@ -4,15 +4,21 @@
 
 int main()
 {
-	int a = 5;
-	int b = 2;
-	std::cout << "a = " << a << std::endl;
-	std::cout << "b = " << b << std::endl;
-	swap(a, b);
-	std::cout << "a = " << a << std::endl;
-	std::cout << "b = " << b << std::endl;
-	std::cout << "min(2, 5) = " << min(a, b) << std::endl;
-	std::cout << "max(2, 5) = " << max(a, b) << std::endl;
+	{
+		int a = 2;
+		int b = 3;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+
 	std::cout << "====================== "  << std::endl;
 
 	float f1 = 2.1;
